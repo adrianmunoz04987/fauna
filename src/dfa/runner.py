233eb8b3124464f0ce090b73471c8af658d97741
JoaoArauz <joao.arauz.rodriguez@est.una.ac.cr>
2026-04-21 17:28:13 -0600

    Update runner.py

--- src/dfa/runner.py
+++ src/dfa/runner.py
@@ -9,36 +9,34 @@ Autores:
    Joao Arauz Rodriguez
 """
 
-
-
 def run_dfa(dfa, cadena):
     estado_actual = dfa.start_state
 
-    for simbolo in cadena: # Si el simbolo no esta en el alfabeto, la cadena es rechazada
-            if simbolo not in dfa.alphabet:
-               print(f"  Simbolo '{simbolo}' no esta en el alfabeto.")
-               return False
+    for simbolo in cadena:
+        # Verificar si el símbolo pertenece al alfabeto
+        if simbolo not in dfa.alphabet:
+            print(f"  Simbolo '{simbolo}' no esta en el alfabeto.")
+            return False
 
         siguiente = None
 
-        for t in dfa.transitions: # Buscamos la transicion que corresponde al estado actual y al simbolo
+        # Buscar transición válida
+        for t in dfa.transitions:
             if t.from_state == estado_actual and t.symbol == simbolo:
                 siguiente = t.to_state
                 break
 
-
-        if siguiente is None: # Si no hay transicion definida se rechaza la cadena
+        # Si no hay transición → rechaza
+        if siguiente is None:
             print(f"  No hay transicion desde {estado_actual} con '{simbolo}'.")
             return False
-
 
         print(f"  {estado_actual} --{simbolo}--> {siguiente}")
         estado_actual = siguiente
 
-
-    for state in dfa.states: # Se revisa si el estado donde quedamos es de aceptacion
+    # Verificar si termina en estado de aceptación
+    for state in dfa.states:
         if state.id == estado_actual and state.accepting:
             return True
 
-
-    return False # Si no es de aceptacion, la cadena es rechazada
+    return False
